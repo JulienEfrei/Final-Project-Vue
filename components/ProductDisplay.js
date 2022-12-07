c4576de2677e42c7a8fd00884e99b71fe3b85efc
Julien Klein <44435573+iLdro@users.noreply.github.com>
2022-12-07 18:43:37 +0100

    Added css
    Co-authored-by: Gaëtan <EniGmaaaaA@users.noreply.github.com>

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,102 +1,128 @@
-app.component('product-display', {
+app.component("product-display", {
   props: {
     premium: {
       type: Boolean,
-      required: true
-    }
+      required: true,
+    },
   },
   template:
     /*html*/
     `
-   <div class="product-display">
+    <div class="container">
+    <div class="product-display">
         
     <div class="product-container">
-      <div class="product-image">
-        <img :src="image" />
-      </div>
-
-      <div class="product-info">
-        <h1>{{ productName }}</h1>
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-        <p>Shipping: {{ shipping }}</p>
-
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-
-        <div class="color-circle"
-          v-for="(variant, index) in variants" 
-          :key="variant.id"
-          :style="{ backgroundColor: variant.color }"
-          @mouseover="updateProduct(index)"
-          >
-        </div> 
-
-        <button class="button" v-on:click="addToCart" 
-          :disabled="!inStock"
-          :class="{ disabledButton: !inStock }"
-          >
-        Add to cart
-        </button>
-      </div>
+    <div class="product-image">
+    <img :src="image" />
     </div>
-
-    <review-list :reviews="reviews"></review-list>
-    <review-form @review-submitted="addReview" ></review-form>
-  </div>
-   `,
+    <div class="buttons">
+          <div class="color-circle"
+            v-for="(variant, index) in variants" 
+            :key="variant.id"
+            :style="{ backgroundColor: variant.color , border: variant.color}"
+            @mouseover="updateProduct(index)"
+            >
+          </div> 
+        </div>
+        <div class="product-title">
+        <h1>{{ productName }}</h1>
+        </div>
+        <div class="product-info">
+          <div class="main_info_1">
+          <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <p> Price (free taxes): {{ price }}</p>
+            <p>Shipping: {{ shipping }}</p>
+          </div>
+          <div class="main_info_2">
+          <ul>
+              <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+          </div>
+          </div>
+        <div class="product-tabs">
+        <button class="button" v-on:click="addToCart" 
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }"
+            >
+            <span>
+              Add to cart
+            </span>
+            </button>
+            </div>
+            <div class="review">
+            <review-form @review-submitted="addReview" ></review-form>
+            </div>
+            <review-list :reviews="reviews"></review-list>
+            </div>
+      </div>
+      </div>
+      
+  `,
   data() {
     return {
-      product: 'Socks',
-      brand: 'Vue Mastery',
+      product: "Cars",
+      brand: "Bugatti",
       selectedVariant: 0,
-      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      price: "4 800 000",
+      details: [
+        "French quality product",
+        "W16 8.0L of 1 479HP",
+        "0-100 Km/h in 2.4s",
+        "2 seats",
+        "Mixt consommation: 16.6 - 26.8L/100Km",
+      ],
       variants: [
         {
           id: 2234,
-          color: 'green',
-          image: './assets/images/socks_green.jpg',
-          quantity: 10
+          color: "red",
+          image: "../assets/images/bugatti_chiron_rouge.jpg",
+          quantity: 10,
         },
         {
           id: 2235,
-          color: 'blue',
-          image: './assets/images/socks_blue.jpg',
-          quantity: 0
-        }
+          color: "black",
+          image: "../assets/images/bugatti_chiron_noir.jpg",
+          quantity: 0,
+        },
+        {
+          id: 2236,
+          color: "#14509d",
+          image: "../assets/images/bugatti_chiron_bleu.jpg",
+          quantity: 2,
+        },
       ],
       reviews: [],
-      tabs: ['review-form', 'review-list'],
-      activeTab: 'review-form'
-    }
+      tabs: ["review-form", "review-list"],
+      activeTab: "review-form",
+    };
   },
   methods: {
     addToCart() {
-      this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+      this.$emit("add-to-cart", this.variants[this.selectedVariant].id);
     },
     updateProduct(index) {
-      this.selectedVariant = index
+      this.selectedVariant = index;
     },
     addReview(review) {
-      this.reviews.push(review)
-    }
+      this.reviews.push(review);
+    },
   },
   computed: {
     productName() {
-      return this.brand + ' ' + this.product
+      return this.brand + " " + this.product;
     },
     image() {
-      return this.variants[this.selectedVariant].image
+      return this.variants[this.selectedVariant].image;
     },
     inStock() {
-      return this.variants[this.selectedVariant].quantity
+      return this.variants[this.selectedVariant].quantity;
     },
     shipping() {
       if (this.premium) {
-        return 'Free'
+        return "Free";
       }
-      return 2.99
-    }
-  }
-})
+      return 2.99;
+    },
+  },
+});
